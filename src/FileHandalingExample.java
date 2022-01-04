3dc53649781ebdc21a26b5374953f10108eeff35
Ashish Gautam <agautam@hamropatro.com>
2022-01-04 06:45:48 +0545

    read, write and delete from file

--- src/FileHandalingExample.java
+++ src/FileHandalingExample.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileHandalingExample {
 
@@ -11,6 +14,36 @@ public class FileHandalingExample {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    void writeDataToFile(){
+        try {
+            FileWriter fileWriter=new FileWriter("example.txt");
+            fileWriter.write("I am good boy");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void readContentFromFile(){
+        File file=new File("example.txt");
+        try {
+            Scanner readFile=new Scanner(file);
+            while (readFile.hasNextLine()){
+                String fileContent=readFile.nextLine();
+                System.out.println(fileContent);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    void deleteFile(){
+        File file=new File("example.txt");
+        if(file.delete()){
+            System.out.println(file.getName()+"is Deleted");
         }
     }
 }
